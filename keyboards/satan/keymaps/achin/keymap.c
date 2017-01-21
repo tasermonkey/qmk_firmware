bea38e63c1666c3ddda0a6d405f5632da31897a7
Alex Chin <alex.ba.chin@gmail.com>
2017-01-21 11:14:25 -0500

    Added delete key to function layer.

--- keyboards/satan/keymaps/achin/keymap.c
+++ keyboards/satan/keymaps/achin/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* function layer */
 [_FL] = KEYMAP_ANSI(
-  KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F8,  KC_F10, KC_F11, KC_F12, _______, \
+  KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F8,  KC_F10, KC_F11, KC_F12, KC_DEL , \
   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
   _______,_______,KC_PGUP,KC_HOME,KC_END, KC_PGDN,KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,_______,_______,        _______, \
   _______,        KC_MUTE,KC_VOLD,KC_VOLU,_______,_______,_______,_______,_______,_______,_______,        _______, \
